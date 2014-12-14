28395309ab93b64eb063669748a7885e3a2bbe69
Steven Birkner Jr <Steve@Stevens-MacBook-Pro.local>
2014-12-14 13:38:54 -0500

    little tweaks

--- js/main.js
+++ js/main.js
@@ -1,16 +1,44 @@
-function search() {
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'http://localhost:9200/sentiment/_search', false);
-	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded","Access-Control-Allow-Origin","*");
 
+function search() {
+	
 	var searchText = document.getElementById('searchText').value,
 		author = document.getElementById('author').value;
-	
-	xhr.send("q=" + searchText);
-    if(xhr.readyState === 4 && xhr.status === 200) {
-        if(xhr.responseText !== "") {
-            json = JSON.parse(xhr.responseText);
-            console.log(json);                  	                 
-        }
-    }
+	console.log(searchText,author);
+	if (author=='none') {
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'http://localhost:9200/sentiment/_search?q='+ searchText, false);
+		xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded","Access-Control-Allow-Origin","*");
+
+		xhr.send();
+    	if(xhr.readyState === 4 && xhr.status === 200) {
+        	if(xhr.responseText !== "") {
+           		json = JSON.parse(xhr.responseText);
+           		var temp = json.hits.hits;
+           		for(var i=0;i<temp.length; i++) {
+           			console.log(temp[i]._source['author'],temp[i]._source['message']);
+           			$('#answers').append('<li><b>'+temp[i]._source['author']+': </b> '+temp[i]._source['message']+'</li>');
+           		}                 	                 
+        	}
+    	}
+	} else if(author != 'None' && searchText == ''){
+		console.log('here');
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'http://localhost:9200/sentiment/_search?q=author:'+ author, false);
+		xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded","Access-Control-Allow-Origin","*");
+		xhr.send();
+    	if(xhr.readyState === 4 && xhr.status === 200) {
+        	if(xhr.responseText !== "") {
+           		json = JSON.parse(xhr.responseText);
+            	var temp = json.hits.hits;
+            	console.log(temp);
+           		for(var i=0;i<temp.length; i++) {
+           			console.log(temp[i]._source['author'],temp[i]._source['message']);
+           			$('#answers').append('<li><b>'+temp[i]._source['author']+': </b> '+temp[i]._source['message']+'</li>');
+           		}                            	                 
+        	}
+    	}
+	} else {
+
+	}
+
 }
